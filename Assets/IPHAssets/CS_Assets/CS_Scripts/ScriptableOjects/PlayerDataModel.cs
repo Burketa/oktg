f8e8cebdc7c7c9f427067cc1a97de5db4b79ef7a
burca <rlpelizzon@gmail.com>
2019-10-10 18:27:29 -0300

    adicionado prototipo da camada de persistencia

---
+++ Assets/IPHAssets/CS_Assets/CS_Scripts/ScriptableOjects/PlayerDataModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class PlayerDataModel
+{
+    public PlayerDataModel() { }
+    public PlayerDataModel(int lastScore, int selectedCharacter)
+    {
+        this.lastScore = lastScore;
+        this.selectedCharacter = selectedCharacter;
+    }
+
+    //*Player Stats
+    public int lastScore = 0;
+
+    //Which character is selected to play
+    public int selectedCharacter = 0;
+
+    //Number os feathers collected from all games
+    public int feathersCollected = 0;
+
+    //Default ammount of highscores to keep track of
+    public int topScoresAmmount = 10;
+
+    //Top 10 highscores
+    public List<int> topScores = new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+    //*Som
+    //Sound and FX volume
+    public float volume = 0.5f;
+
+    //May the game play FX Sounds ?
+    public bool canPlayFX = true;
+
+    //May the game play Music Sounds ?
+    public bool canplayMusic = true;
+
+    //Achievements
+    //Progressao e status dos achievs
+}
--- Assets/IPHAssets/CS_Assets/CS_Scripts/Types/PlayerData.cs
+++ Assets/IPHAssets/CS_Assets/CS_Scripts/Types/PlayerData.cs
@@ -1,19 +1,123 @@
-﻿public static class PlayerData
+﻿using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+public class PlayerData : MonoBehaviour
 {
-    public static PlayerDataScriptableObject playerData;
+    private static PlayerDataModel playerData;
+    private static string dataPath;
+    public void Start()
+    {
+        dataPath = Path.Combine(Application.persistentDataPath, "PlayerData.json");
+        playerData = LoadPlayerData(dataPath);
+    }
 
-    //Reset the persistency data to default values
+    //Reset the persistence data to default values
     public static void ResetProfile()
     {
+        playerData = new PlayerDataModel();
+
         playerData.lastScore = 0;
         playerData.selectedCharacter = 0;
         playerData.feathersCollected = 0;
+        playerData.topScoresAmmount = 10;
 
-        for (int i = 0; i < 10; i++)
-            playerData.top10Scores[i] = 0;
+        playerData.topScores = new List<int>();
+        for (int i = 0; i < playerData.topScoresAmmount; i++)
+            playerData.topScores.Add(0);
 
         playerData.volume = 0.5f;
-        playerData.canplayMusic = false;
-        playerData.canPlayFX = false;
+        playerData.canplayMusic = true;
+        playerData.canPlayFX = true;
+
+        SavePlayerData(playerData, dataPath);
+    }
+
+    //Put the highscore in the list, if it is indeed a highscore and trim the list to topScoresAmmount size
+    public static void CheckHighscore(int possibleHighscore)
+    {
+        int index = 0;
+
+        foreach (int score in new List<int>(playerData.topScores))
+        {
+            if (possibleHighscore >= score)
+            {
+                playerData.topScores.Insert(index, possibleHighscore);
+                playerData.topScores = playerData.topScores.GetRange(0, playerData.topScoresAmmount);
+                SavePlayerData(playerData, dataPath);
+                break;
+            }
+            index++;
+        }
+    }
+
+    public static void SavePlayerData(PlayerDataModel data, string path)
+    {
+        string jsonString = JsonUtility.ToJson(data);
+
+        using (StreamWriter streamWriter = File.CreateText(path))
+        {
+            streamWriter.Write(jsonString);
+        }
+    }
+
+    public static PlayerDataModel LoadPlayerData(string path)
+    {
+        if (!File.Exists(path))
+        {
+            SavePlayerData(new PlayerDataModel(), path);
+        }
+
+        using (StreamReader streamReader = File.OpenText(path))
+        {
+            string jsonString = streamReader.ReadToEnd();
+            return JsonUtility.FromJson<PlayerDataModel>(jsonString);
+        }
     }
 }
+/*
+
+
+using System.IO;
+using UnityEngine;
+
+public class JsonCharacterSaver : MonoBehaviour
+{
+    public CharacterData characterData;
+    string dataPath;
+
+    void Start ()
+    {
+        dataPath = Path.Combine(Application.persistentDataPath, "CharacterData.txt");
+    }
+
+    void Update ()
+    {
+        if(Input.GetKeyDown (KeyCode.S))
+            SaveCharacter (characterData, dataPath);
+
+        if (Input.GetKeyDown (KeyCode.L))
+            characterData = LoadCharacter (dataPath);
+    }
+
+    static void SaveCharacter (CharacterData data, string path)
+    {
+        string jsonString = JsonUtility.ToJson (data);
+
+        using (StreamWriter streamWriter = File.CreateText (path))
+        {
+            streamWriter.Write (jsonString);
+        }
+    }
+
+    static CharacterData LoadCharacter (string path)
+    {
+        using (StreamReader streamReader = File.OpenText (path))
+        {
+            string jsonString = streamReader.ReadToEnd ();
+            return JsonUtility.FromJson<CharacterData> (jsonString);
+        }
+    }
+}
+
+
+ */
