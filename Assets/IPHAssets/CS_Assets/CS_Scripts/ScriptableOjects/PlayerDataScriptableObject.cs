35e416953a2cf6cbd6971b4bb0a6346fa2b745d2
burca <rlpelizzon@gmail.com>
2019-10-10 17:37:46 -0300

    adicionado scriptable object para persistencia de dados

---
+++ Assets/IPHAssets/CS_Assets/CS_Scripts/ScriptableOjects/PlayerDataScriptableObject.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerData", menuName = "ScriptableObjects/PlayerDataScriptableObject")]
+public class PlayerDataScriptableObject : ScriptableObject
+{
+    //Player Stats
+    //public int playerStats;
+    //Score from last game session
+    public int lastScore;
+    //Which character is selected to play
+    public int selectedCharacter;
+    //Number os feathers collected from all games
+    public int feathersCollected;
+    //Top 10 highscores
+    public int[] top10Scores;
+
+    //Som
+    //Sound and FX volume
+    public float volume;
+    //May the game play FX Sounds ?
+    public bool canPlayFX;
+    //May the game play Music Sounds ?
+    public bool canplayMusic;
+
+    //Achievements
+    //Progressao e status dos achievs
+}
+
+//?fazer com que sejam ox X highscores ?
+//?Colocar com Regions ?
---
+++ Assets/IPHAssets/CS_Assets/CS_Scripts/Types/PlayerData.cs
@@ -0,0 +1,19 @@
+﻿public static class PlayerData
+{
+    public static PlayerDataScriptableObject playerData;
+
+    //Reset the persistency data to default values
+    public static void ResetProfile()
+    {
+        playerData.lastScore = 0;
+        playerData.selectedCharacter = 0;
+        playerData.feathersCollected = 0;
+
+        for (int i = 0; i < 10; i++)
+            playerData.top10Scores[i] = 0;
+
+        playerData.volume = 0.5f;
+        playerData.canplayMusic = false;
+        playerData.canPlayFX = false;
+    }
+}
