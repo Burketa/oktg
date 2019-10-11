6c2414d1b1fffd741e92ba4635c25015ab2c4bb3
burca <rlpelizzon@gmail.com>
2019-10-10 22:14:58 -0300

    adicionado highscores dinamicos

---
+++ Assets/IPHAssets/CS_Assets/CS_Scripts/IPHHighscores.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IPHHighscores : MonoBehaviour
+{
+    public GameObject highScorePrefab;
+
+    public Transform contentParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerDataModel.PlayerStats playerStats = PlayerData.playerData.playerStats;
+
+        for (int i = 0; i < playerStats.topScoresAmmount; i++)
+        {
+            GameObject score = (GameObject)Instantiate(highScorePrefab, Vector2.zero, Quaternion.identity, contentParent);
+            score.transform.localScale = Vector3.one;
+            score.GetComponent<Text>().text = $"{i + 1} - {playerStats.topScores[i]}";
+        }
+    }
+}
--- Assets/IPHAssets/CS_Assets/CS_Scripts/IPHStatistics.cs
+++ Assets/IPHAssets/CS_Assets/CS_Scripts/IPHStatistics.cs
@@ -1,21 +1,20 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 
 public class IPHStatistics : MonoBehaviour
 {
     // Use this for initialization
     void Start()
     {
-        //PlayerData data = new PlayerData();
+        PlayerDataModel.PlayerStats playerStats = PlayerData.playerData.playerStats;
 
         // Set the statistics values in the statistics canvas
-        GameObject.Find("TextDistance").GetComponent<Text>().text = "LONGEST DISTANCE: " + PlayerData.playerData.playerStats.topScores[0];
-        GameObject.Find("TextStreak").GetComponent<Text>().text = "LONGEST STREAK: " + PlayerData.playerData.playerStats.longestStreak;
-        GameObject.Find("TextTokens").GetComponent<Text>().text = "TOTAL FEATHERS: " + PlayerData.playerData.playerStats.feathersCollected;
-        GameObject.Find("TextPowerups").GetComponent<Text>().text = "TOTAL POWERUPS: " + PlayerData.playerData.playerStats.powerupsCollected;
+        GameObject.Find("TextDistance").GetComponent<Text>().text = "LONGEST DISTANCE: " + playerStats.topScores[0];
+        GameObject.Find("TextStreak").GetComponent<Text>().text = "LONGEST STREAK: " + playerStats.longestStreak;
+        GameObject.Find("TextTokens").GetComponent<Text>().text = "TOTAL FEATHERS: " + playerStats.feathersCollected;
+        GameObject.Find("TextPowerups").GetComponent<Text>().text = "TOTAL POWERUPS: " + playerStats.powerupsCollected;
         GameObject.Find("TextPowerupStreak").GetComponent<Text>().text = "LONGEST POWERUP: ??"; // ?
-        GameObject.Find("TextCharacters").GetComponent<Text>().text = "CHARACTERS UNLOCKED: " + PlayerData.playerData.playerStats.charactersUnlocked;
+        GameObject.Find("TextCharacters").GetComponent<Text>().text = "CHARACTERS UNLOCKED: " + playerStats.charactersUnlocked;
     }
 
 }
